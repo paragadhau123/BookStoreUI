4faf0ace08db13888ed0d466e31455d051acd9ac
Parag Adhau <adhauparag64@gmail.com>
2020-12-06 00:33:04 +0530

    Added[Parag]:reset password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 import { LoginComponent } from './components/login/login.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
   {path:"toolbar",component:ToolbarComponent},
-  {path:"forgotPassword",component:ForgotpasswordComponent}
+  {path:"forgotPassword",component:ForgotpasswordComponent},
+  {path:"resetPassword",component:ResetpasswordComponent}
 ];
 
 @NgModule({
--- src/app/components/resetpassword/resetpassword.component.ts
+++ src/app/components/resetpassword/resetpassword.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ResetpasswordComponent implements OnInit {
 
   constructor() { }
-
+  hide = true;
+  errors;
   ngOnInit(): void {
   }
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  getPasswordErrorMessage(){
 
+    if (this.password.hasError('required')) {
+      return 'Please enter a password';
+    }
+   return this.password.invalid ? 'Password length must be greater than 6' : '';
+  }
 }
