86a37e087d5a5f5a212b408614bb7dd8cb290a15
Parag Adhau <adhauparag64@gmail.com>
2020-12-07 20:49:13 +0530

    Refactored[Parag]:toolbar component for hiding search bar in admin dashboard

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -9,13 +9,16 @@ export class ToolbarComponent implements OnInit {
   constructor() { }
   @Input() childMessage: string;
   dispalyimg=null
+  dispalySearchBar=null
   ngOnInit(): void {
     if (this.childMessage == "Admin"){
       this.dispalyimg=false;
+     this.dispalySearchBar=false;
     
     }
     else{
       this.dispalyimg=true;
+      this.dispalySearchBar=true;
     }
   }
 
